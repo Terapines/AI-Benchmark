3e3efc4946ac6379b3037bca02f86e550e3149f6
Luke <leguang.liu@terapines.com>
2024-09-27 10:46:39 +0800

    Optimize matmul c kernel (loop order)

--- src/c/matmul.cpp
+++ src/c/matmul.cpp
@@ -14,14 +14,21 @@ void matmul(float *arg0, float *arg1, float *arg2, int M, int N, int K) {
   if (getBoolEnv("TRITON_CPU_OMP_DEBUG"))
     printf("max_threads: %d\n", max_threads.value());
 
-    // For now, use the default chunk size, total iterations / max_threads.
+  // For now, use the default chunk size, total iterations / max_threads.
 #pragma omp parallel for schedule(static) num_threads(max_threads.value())
   for (int i = 0; i < M; i++) {
     for (int j = 0; j < N; j++) {
       arg2[i * N + j] = 0;
-      for (int k = 0; k < K; k++) {
+    }
+  }
+
+#pragma omp parallel for schedule(static) num_threads(max_threads.value())
+  for (int i = 0; i < M; i++) {
+    for (int k = 0; k < K; k++) {
+      for (int j = 0; j < N; j++) {
         arg2[i * N + j] += arg0[i * K + k] * arg1[k * N + j];
       }
     }
   }
+
 }
